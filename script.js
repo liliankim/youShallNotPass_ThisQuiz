d9d7ede2db27a27cea7a4ccb26f9094acf704b74
liliankim <yejean831@gmail.com>
2022-05-21 21:36:29 -0700

    added variables for question/answer

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+var questions = [
+    {
+        ask: 'What tool in Javascript do we use to debug our code?',
+        choice: {
+            a: 'query selector',
+            b: 'console log',
+            c: 'console track',
+        },
+        answer: 'b'
+        }
 
+        {
+            ask: 'What is not a Boolean Value',
+            choice: {
+                a: 'Yes',
+                b: 'No',
+                c: 'Maybe',
+            },
+            answer: 'c'
+            }
+
+            {
+                ask: 'What represents equal value and equal type?',
+                choice: {
+                    a: '===',
+                    b: '!=',
+                    c: '>=',
+                },
+                answer: 'a'
+                }
+
+
+    
+
+]
